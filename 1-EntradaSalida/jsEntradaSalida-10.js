98388483a2ee0e36acd57472e4302e97d6afc3f5
AgusFerro <32740497+AgusFerro@users.noreply.github.com>
2019-07-03 19:53:50 -0300

    Add files via upload
    Entrada/Salida completo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+    var importe;
+    var descuento;
+    var importeNuevo;
+
+    importe = parseInt(document.getElementById("importe").value);
+
+    descuento = importe * 0.25;
+
+    importeNuevo = importe - descuento;
+
+    document.getElementById("resultado").value = importeNuevo;
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -2,8 +2,18 @@
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
+var sueldo;
+var aumento;
+var sueldoNuevo;
+
 function mostrarAumento()
 {
+    sueldo = parseInt(document.getElementById("sueldo").value);
 
-	
+    aumento = sueldo * 0.10;
+
+    sueldoNuevo = aumento + sueldo;
+
+    document.getElementById("resultado").value = sueldoNuevo;
+
 }
