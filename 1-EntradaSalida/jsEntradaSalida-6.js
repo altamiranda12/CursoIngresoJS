477a8d04fbf01f3c8dfcf36360d3a8c57b651e27
AgusFerro <32740497+AgusFerro@users.noreply.github.com>
2019-07-03 19:30:18 -0300

    Add files via upload
    Hasta ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var num1;
+    var num2;
+    var num3;
+
+    num1 = parseInt(document.getElementById("numeroUno").value);
+
+    num2 = parseInt(document.getElementById("numeroDos").value);
+
+    num3 = num1 + num2;
+
+    alert("el resultado es " + num3);
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,40 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+var num1;
+var num2;
+var num3;
+
 function sumar()
 {	
-	
+    num1 = parseInt(document.getElementById("numeroUno").value);
+    num2 = parseInt(document.getElementById("numeroDos").value);
+    num3 = num1 + num2;
+    alert("el resultado es " + num3);
 }
 
 function restar()
 {
-	
+    num1 = parseInt(document.getElementById("numeroUno").value);
+    num2 = parseInt(document.getElementById("numeroDos").value);
+    num3 = num1 - num2;
+    alert("el resultado es " + num3);
 }
 
 function multiplicar()
 { 
-	
+    num1 = parseInt(document.getElementById("numeroUno").value);
+    num2 = parseInt(document.getElementById("numeroDos").value);
+    num3 = num1 * num2;
+    alert("el resultado es " + num3);
 }
 
 function dividir()
 {
-	
+    num1 = parseInt(document.getElementById("numeroUno").value);
+    num2 = parseInt(document.getElementById("numeroDos").value);
+    num3 = num1 / num2;
+    alert("el resultado es " + num3);
 }
 
